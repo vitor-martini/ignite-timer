f2fc2ace29c9f975aee744bfe24750f4c572ad16
Vitor Martini <vitor-martini@outlook.com>
2024-09-17 21:57:18 -0300

    fix: date issues

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useReducer, useState } from 'react';
+import { createContext, ReactNode, useEffect, useReducer, useState } from 'react';
 import { CreateCycleData, Cycle } from '../interfaces/cycle';
 import { cyclesReducer } from '../reducers/cycles/reducer';
 import { 
@@ -6,6 +6,7 @@ import {
   interruptCurrentCycleAction, 
   markCurrentCycleAsFinishedAction 
 } from '../reducers/cycles/actions';
+import { differenceInSeconds } from 'date-fns';
 
 interface CyclesContextProviderProps {
   children: ReactNode
@@ -25,13 +26,41 @@ interface CyclesContextType {
 export const CyclesContext = createContext({} as CyclesContextType);
 
 export function CyclesContextProvider({ children }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer, 
+    {
     cycles: [],
     activeCycleId: null,
-  });
+    }, 
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem('@ignite-timer:cycles-state-1.0.0');
+      if(storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return initialState;
+    }
+  );
 
   const { cycles, activeCycleId } = cyclesState;
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  const activeCycle = cycles.find(c => c.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if(activeCycle) {
+      return differenceInSeconds(
+        new Date(), 
+        new Date(activeCycle.startDate)
+      );
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON);
+  }, [cyclesState]);
+
 
   function markCurrentCycleAsFinished() {
     dispatch(markCurrentCycleAsFinishedAction());
@@ -56,8 +85,6 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   function interruptCurrentCycle() {
    dispatch(interruptCurrentCycleAction());
   }
-
-  const activeCycle = cycles.find(c => c.id === activeCycleId);
   
   return (
     <CyclesContext.Provider 
